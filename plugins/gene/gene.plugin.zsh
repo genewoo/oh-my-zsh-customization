0632631494a6c699ea46e796bc2203f06e5ad62c
Gene Wu <genewoo@gmail.com>
2012-01-03 22:27:01 +0800

    add ws and dropb command

--- plugins/gene/gene.plugin.zsh
+++ plugins/gene/gene.plugin.zsh
@@ -10,6 +10,22 @@ mvim () {
     open -a macvim "$@"
 }
 
+dropb () {
+  cd ~/Dropbox/$1
+}
+
+_dropb() { _files -W ~/Dropbox -/; }
+
+compdef _dropb dropb
+
+ws () {
+  cd ~/Workspaces/$1
+}
+
+_ws() { _files -W ~/Workspaces -/; }
+
+compdef _ws ws
+
 alias la='ls -a'
 alias gsf='git svn fetch'
 alias gsr='git svn rebase'
